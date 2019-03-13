ff1c26ce8afafc964fd1ab8941ae63fbc553a750
An SangKil <mycup@nate.com>
2019-03-13 14:59:51 +0900

    Initial commit

---
+++ src/main/java/com/cjenm/tracking/hctrackingserver/HcTrackingServerApplication.java
@@ -0,0 +1,13 @@
+package com.cjenm.tracking.hctrackingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcTrackingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcTrackingServerApplication.class, args);
+	}
+
+}
